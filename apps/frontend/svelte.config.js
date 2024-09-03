ff74d89bbf7b085b5ec6367aca2ddc429c44f776
FelixKLG <felix@felixklg.dev>
2024-09-03 17:13:15 +0100

    Update SvelteKit to work better as an SPA.

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
+			pages: "build",
+			assets: "build",
+			fallback: "index.html",
 			precompress: false,
-			strict: true
+			strict: true,
 		}),
 		alias: {
-			"$lib/*": "./src/lib/*"
-		}
+			"$lib/*": "./src/lib/*",
+		},
 	},
 };
 
